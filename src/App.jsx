844119be1563e3cc83ba5935ff8f38eb26ba23d3
kira07sj <kirubeldagne1@gmail.com>
2024-12-03 00:04:04 +0300

    Update: navbar on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Navbar from "./components/Navbar"
+import Navigation from "./components/Navigation.jsx"
 import Hero from "./components/Hero"
 function App() {
   
   return (
     <div className="flex flex-col items-center  w-[100dvw] h-[100dvh]">
-    <Navbar/>
+    <Navigation/>
     <Hero/>
 
     </div>
