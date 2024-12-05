7bd14911aa132544b722e81954df14c15594c808
kira07sj <kirubeldagne1@gmail.com>
2024-12-05 15:27:28 +0300

    Add: Next button

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Card from './Card'
 import { ProductInfo } from '../constants'
+import Button from './Button'
 
 const Products = () => {
   return (
@@ -12,7 +13,22 @@ const Products = () => {
             {ProductInfo.map((items)=>(
               <Card key={items.id} title={items.title} Price={items.price} imgUrl={items.imageUrl}/>
             ))}
-        </div>        
+        </div>   
+
+        <div className='w-full flex flex-row gap-3 items-center justify-center my-4'>
+            <Button title={'Previous'} className={`w-[100px] h-[45px] rounded-lg card_border`}/>
+            <div className='w-[30px] h-[30px] bg-dark-brown flex items-center justify-center rounded-md card_border'>
+              <p className='text-white font-semibold'>1</p>
+            </div>
+
+            <div className='w-[30px] h-[30px]  flex items-center justify-center rounded-md card_border'>
+              <p className='text-dark-brown font-semibold'>2</p>
+            </div>
+            <div className='w-[30px] h-[30px]  flex items-center justify-center rounded-md card_border'>
+              <p className='text-dark-brown font-semibold'>3</p>
+            </div>
+            <Button title={'Next'} className={`w-[100px] h-[45px] rounded-lg card_border`}/>
+        </div>     
     </section>
   )
 }
