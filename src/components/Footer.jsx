79945ae4b70656aadb685e74d3b4f77280793348
kira07sj <kirubeldagne1@gmail.com>
2024-12-06 21:32:23 +0300

    Add: Footer 100% Completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,22 @@
 import React from 'react'
+import { FooterLinks } from '../constants'
 
 const Footer = () => {
   return (
    <section className='w-[100dvw] z-40 mt-[5rem] '>
         <div className='w-full  bg-dark-brown flex max-md:flex-col items-start justify-around   p-[2rem]'>
 
-            <div className='flex flex-col items-start justify-center'>
-                <a href="#Home"><h1 className='text-white font-bold text-[2rem] max-md:text-[1.5rem]'>Teke Habesha</h1></a>
-                <a href="#About" className='text-white my-2'>About Us</a>
-                <a href="#About" className='text-white my-2'>Our Team</a>
-                <a href="#Products" className='text-white my-2'>Products</a>
-            </div>
+            {FooterLinks.map((items)=>(
 
-            <div className='flex flex-col items-start justify-center'>
-                <a href="#Home"><h1 className='text-white font-bold text-[1.6rem] max-md:text-[1.3rem]'>Support</h1></a>
-                <a href="https://t.me/Kira07sj" className='text-white my-2'>Help Center</a>
-                <a href="https://t.me/Kira07sj" className='text-white my-2'>FAQs</a>
-                <a href="https://t.me/Kira07sj" className='text-white my-2'>Privecy Policy</a>
-                <a href="https://t.me/Kira07sj" className='text-white my-2'>Terms of Service</a>
-                
-            </div>
-            
-            <div className='flex flex-col items-start justify-center'>
-                <a href="#Home"><h1 className='text-white font-bold text-[2rem] max-md:text-[1.5rem]'>Teke Habesha</h1></a>
-                <a href="#About" className='text-white my-2'>About Us</a>
-                <a href="#About" className='text-white my-2'>Our Team</a>
-                <a href="#Products" className='text-white my-2'>Products</a>
-            </div>
-            
+                <div key={items.id} className='flex flex-col items-start justify-center'>
+                <a href="#Home"><h1 className='text-white font-bold text-[1.6rem] max-md:text-[1.2rem]'>{items.title}</h1></a>
+                {items.TheLinks.map((Links)=>(
+                    <a key={Links.id} href={Links.link} className='text-white my-2'>{Links.title}</a>
+                ))}
+                </div>
+
+            ))}
+           
         </div>
    </section>
   )
--- src/constants/index.js
+++ src/constants/index.js
@@ -17,3 +17,43 @@ export const ProductInfo = [
     {id:'8', title:'Habesha Kemis',imageUrl:'/assets/products/8.jpg',price:'6000' },
     
 ]
+
+export const FooterLinks = [
+    {
+        id:'1',
+        title:'Teke Habesha',
+        TheLinks:[
+            { id:'1', title:'About Us', link:'#About'},
+            { id:'2', title:'Our Team', link:'#About'},
+            { id:'3', title:'Products', link:'#Product'},
+            
+        ]
+
+    },
+
+    {
+        id:'2',
+        title:'Support',
+        TheLinks:[
+            { id:'1', title:'Help Center', link:'https://t.me/Tekehabesha'},
+            { id:'2', title:'FAQs', link:'https://t.me/Tekehabesha'},
+            { id:'3', title:'Privacy Policy', link:'https://t.me/Tekehabesha'},
+            { id:'4', title:'Terms of Service', link:'https://t.me/Tekehabesha'},
+            
+            
+        ]
+
+    },
+
+    {
+        id:'3',
+        title:'Follow Us',
+        TheLinks:[
+            { id:'1', title:'Facebook', link:'#About'},
+            { id:'2', title:'Instagram', link:'#About'},
+            { id:'3', title:'Telegram', link:'https://t.me/Tekehabesha'},
+            
+        ]
+
+    },
+]
