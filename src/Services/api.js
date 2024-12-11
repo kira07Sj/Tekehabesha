55ddf797bbc6ca465708a5f66b7c72a24342c8e0
kira07sj <kirubeldagne1@gmail.com>
2024-12-11 13:30:16 +0300

    Add: getProduct api Completed

--- src/Services/api.js
+++ src/Services/api.js
@@ -3,5 +3,13 @@ import axios from "axios";
 const API_Url = "http://127.0.0.1:8000/app"
 
 export const getProducts = async () => {
-    const res = await axios.get('${API_Url}/products/')
+    try {
+        const res = await axios.get('${API_Url}/products/')
+        return res.data // this will return the list of the products
+    } catch (error) {
+        console.error("Error fatching the data", error);
+        throw error
+    }
+
+
 }
