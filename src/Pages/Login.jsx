98e045d71becdb460b2009111c187cf8283cb86b
kira07sj <kirubeldagne1@gmail.com>
2025-02-12 21:23:36 +0300

    Update: login interface

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,6 +2,8 @@ import React, {useState, useContext} from 'react'
 import { AuthContext } from '../auth/AuthContext'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import tilet from '../assets/tilet2.png'
+import image3 from '../assets/image3.png'
 
 const Login = () => {
 
@@ -23,12 +25,22 @@ const Login = () => {
     };
 
   return (
-    <div className='w-full h-full'>
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder='username' value={username} onChange={(e) => setUsername(e.target.value)} required />
-            <input type="password" placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} required />
-            <button type='submit'>Login</button>
+    <div className='w-[100%] h-[100dvh] flex items-center justify-start  flex-col lg:flex-row md:px-[3rem]'>
+        <div className=' fixed top-0 right-10 max-md:right-3 -z-10 h-full'>
+                <img src={tilet} alt="bg" 
+                className='h-[615px] object-contain scale-100  lg:scale-150' />
+        </div>
+        <div className='w-60% max-md:w-[90%] h-full flex items-center justify-center flex-col lg:flex-row md:gap-x-[10rem]' >
+        <div className='h-[90%] w-[420px] rounded-lg overflow-hidden max-md:hidden'>
+            <img src={image3} alt="" className='w-full'/>
+        </div>
+        <form onSubmit={handleSubmit} className='flex items-center justify-center flex-col gap-[1.6rem]'>
+            <h1 className='text-color-brown text-[2.4rem] font-bold'>Login to Account</h1>
+            <input type="text" placeholder='username' value={username} onChange={(e) => setUsername(e.target.value)} required className='w-[310px] h-[35px] bg-transparent border rounded-md outline-none'/>
+            <input type="password" placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} required className='w-[310px] h-[35px] bg-transparent border rounded-md outline-none'/>
+            <button type='submit' className='w-[310px] h-[40px] bg-transparent rounded-md outline-none text-white text-[14px] font-semibold btn-bg-gradient'>Log in</button>
         </form>
+        </div>
     </div>
   )
 }
